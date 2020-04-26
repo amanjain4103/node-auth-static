9e7669717fb6a3466cd230a703212c4862e08219
amanjain4103 <amanjain4103@gmail.com>
2020-04-26 23:48:19 +0530

    adding files to git

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const {db} = require('./db');
+var authenticated = false;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+const router = express.Router();
+
+//function to authenticate
+
+const authenticate = (email,password) => {
+
+    for(let i=0;i<db.length;i++){
+
+        if( email == db[i].email  && password == db[i].password ){
+            authenticated = true;
+            break;
+        }else{
+            authenticated = false;
+        }
+    }
+}
+
+//function to add new user
+
+const addMe = (name,email,password)=>{
+    db.push({
+        name,
+        email,
+        password
+    })    
+}
+
+
+//sends login page 
+
+router.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'));
+})
+
+
+//validate user and sends homepage
+
+router.post('/',(req,res)=>{
+
+    let {email,password} = req.body;
+    authenticate(email,password);
+
+    if(authenticated){
+        res.sendFile(path.join(__dirname,"public","home.html"));
+    }else{
+        res.send("wrong credentials")
+    }
+    
+})
+
+
+router.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','register.html'));
+})
+
+router.post('/addUser',(req,res)=>{
+    const {name,email,password} = req.body;
+
+    addMe(name,email,password);
+    res.redirect("/login");
+})
+
+app.use('/',router);
+
+app.listen(3000);
+
---
+++ db.js
@@ -0,0 +1,14 @@
+const db = [
+                {
+                    "name":"Aman",
+                    "email":"aman@gmail.com",
+                    "password" : "1234"
+                },
+                {
+                    "name":"Sharukh",
+                    "email":"srk@gmail.com",
+                    "password" : "5678"
+                }
+            ]
+
+module.exports.db = db;
